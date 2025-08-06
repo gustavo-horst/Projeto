8c282061fc1f84bd2510bc4ceb953c43ac09ba29
Gustavo Wosnes Horst <gustavo.horst.dev@gmail.com>
2025-08-05 21:54:24 -0300

    Criação da classe UsuarioServlet

---
+++ tarefas/src/main/UsuarioServlet.java
@@ -0,0 +1,33 @@
+
+
+@Webservlet(urlP)
+public class UsuarioServlet extends HttpServlet {
+
+	
+	public void init() {
+		dao = new usuarioDAOimpl();
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+
+	protected void doGet() {
+		
+	}
+	
+	protected void doDelete() {
+		
+	}
+
+	protected void doPut() {
+		
+	}
+	
+	switch (methods) {
+	
+	
+	}
+
+}
+
